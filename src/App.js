9bbba856e8bdd1aee8158ce129578d5c38b78838
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-10-20 17:19:16 +0530

    add header css in app file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "./hero.css";
+import "./header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./views/Header";
