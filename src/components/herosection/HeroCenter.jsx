c6e0d4ff874b7e3369895481ce37ce200d163e25
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-10-20 15:44:09 +0530

    two section complite

--- src/components/herosection/HeroCenter.jsx
+++ src/components/herosection/HeroCenter.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Icon, { Icon1 } from "./Icon";
 
 const HeroCenter = () => {
-  return <div></div>;
+  return (
+    <>
+      <section className=" hero_center min-vh-100 d-flex align-items-center justify-content-center">
+        <div className=" container ">
+          <div className=" text-center">
+            <h1 className=" text-center fw-semibold text-white font_md ff_poppins max_width_h1 mx-auto">
+              Unveiling The Soul of{" "}
+              <span className="text_linear_gradient">
+                Every Invention and The Passion
+              </span>{" "}
+              of Those Behind it.
+            </h1>
+            <p className=" ff_poppins fw-normal font-sm lh_sm text-white-50 max_width_parTwo mx-auto pt-2">
+              Ensuring the tales of your relentless research, eureka moments of
+              discovery, and unwavering commitment to patient care neither go
+              untold nor unsold
+            </p>
+            <form className=" mt-4 d-flex justify-content-center align-items-center gap-3">
+              <input
+                type="Email"
+                required
+                className=" bg-transparent  ff_poppins fw-normal font-sm lh_sm text-white-50 w-100 py-3 placeholder_input px-3"
+                placeholder=" Enter your email here and we'll send you some 'magic'..."
+              />
+              <button className="common_btu Do_It_Btn  fw-medium font-sm lh_medium ">
+                Do It
+                <span className=" ms-3">
+                  <Icon1 />
+                </span>
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default HeroCenter;
--- src/components/herosection/HeroHalfContentHalfImage.jsx
+++ src/components/herosection/HeroHalfContentHalfImage.jsx
@@ -1,38 +1,34 @@
 import React from 'react'
 // import IconOne from './IconOne';
-import Icon, { Icon1, PlayButton } from './Icon';
-// import {Frame} from "./src/assets/svg/Frame.svg";
+import Icon, { Icon1 } from './Icon';
+ import Frame from "../../assets/image/svg/Frame.svg";
+ import heroImag from "../../assets/image/png/hero_1.png";
 const HeroHalfContentHalfImage = () => {
   return (
     <>
-      <section className=" py-5">
+      <section className=" py-5 min-vh-100 d-flex align-items-center">
         <div className=" container">
-          <div className=" row">
-            <div className=" col-6">
+          <div className=" row flex-column-reverse flex-md-row align-items-center">
+            <div className="  col-md-6 mt-5">
               <h1 className=" ff_poppins fw-medium font-xl lh_larg text-black">
                 Find your new companion
               </h1>
-              <div className=" mt-3">
-                {/* <img src={Frame} alt="Frame" /> */}
-                <p className=" ff_poppins fw-normal text_primary font-sm max_width_par lh_medium ">
+              <div className=" d-flex align-items-center gap-3 mt-3 mt-lg-5">
+                <img src={Frame} alt="Frame" />
+                <p className=" ff_poppins fw-normal text_primary font-sm max_width_par lh_medium mb-0">
                   Connecting awesome people with a happy healthy kitten from
                   prescreened.
                 </p>
               </div>
-              <div className=" mt-5 pt-3">
-                <button className="common_btu  fw-medium font-sm lh_medium ">
-                  Get Started
-                  <span className=" ms-3">
-                    <Icon1 />
-                  </span>
-                </button>
-                <button className="border-0 fw-medium font-sm lh_medium bg-transparent ms-5 text_linear_gradient">
-                  <span className=" me-2">
-                    <PlayButton />
-                  </span>
-                  Watch Kitten
-                </button>
-              </div>
+              <button className="common_btu  fw-medium font-sm lh_medium mt-5">
+                Get Started
+                <span className=" ms-3">
+                  <Icon1 />
+                </span>
+              </button>
+            </div>
+            <div className=" col-md-6 text-center text-md-start">
+              <img src={heroImag} alt="heroImag" className=" w-100 mx-auto max_width_hero_img" />
             </div>
           </div>
         </div>
