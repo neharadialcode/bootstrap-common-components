c6b434ce4148abf7b8e9f93010a6b6ce9529dc4c
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-10-21 11:23:09 +0530

    all components compliti

---
+++ src/components/herosection/HeroPosition.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import leftImagLeft from "../../assetsFolder/images/png/hero_left_img.png";
+import leftImagRight from "../../assetsFolder/images/png/hero_right _img.png";
+import leftImagRightBottom from "../../assetsFolder/images/png/hero_right _bottom_img.png";
+import leftImagLeftBottom from "../../assetsFolder/images/png/hero_left_bottom_img.png";
+import HeaderRightAlign from "../headersection/HeaderRightAlign";
+
+const HeroPosition = () => {
+  return (
+    <>
+      <div className=" position-relative overflow-hidden">
+        <HeaderRightAlign />
+        <div className="d-flex align-items-center justify-content-center min-vh-100 position-relative">
+          <img
+            src={leftImagLeft}
+            alt="leftImag"
+            className=" position-absolute start-0 top-0 max_w_position_hero_img"
+          />
+          <img
+            src={leftImagRight}
+            alt="leftImagRight"
+            className=" position-absolute end-0 top-0 max_w_position_hero_img"
+          />
+          <img
+            src={leftImagRightBottom}
+            alt="leftImagRight"
+            className=" position-absolute end-0 bottom-0  max_w_position_hero_img"
+          />
+          <img
+            src={leftImagLeftBottom}
+            alt="leftImag"
+            className=" position-absolute start-0 bottom-0  max_w_position_hero_img"
+          />
+          <div className=" d-flex align-items-center justify-content-center">
+            <div className=" max_w_Hero_Position text-center">
+              <span className="ff_poppins font-md text-black">
+                A nice headline
+              </span>
+              <h1 className=" ff_poppins fw-medium font-xl text-black">
+                Goes here
+              </h1>
+
+              <p className=" ff_poppins fw-normal text_primary font-sm   mb-0">
+                Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget
+                consectetur mi. Vulputate sapien a a bibendum suspendisse quam.
+                Hac posuere eget nam diam velit urna adipiscing diam. Dis velit
+                nam vitae hendrerit at egestas. Tellus velit turpis at sed lorem
+                vitae sed.
+              </p>
+
+              <button className="ff_poppins fw-normal font-sm  mx-auto  button_btn mt-5">
+                Button
+              </button>
+              <button className="ff_poppins fw-normal font-sm  mx-auto  button_btn mt-5 ms-3">
+                Button
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeroPosition
--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import HeroCenter from "../components/herosection/HeroCenter";
 import HeroHalfContentHalfImage from "../components/herosection/HeroHalfContentHalfImage";
+import HeroCenterPosition from "../components/herosection/HeroCenterPosition";
+import HeroPositionHalf from "../components/herosection/HeroPositionHalf";
+import HeroPosition from "../components/herosection/HeroPosition";
 
 const Hero = () => {
   return (
     <>
       <HeroCenter />
       <HeroHalfContentHalfImage />
+      <HeroCenterPosition/>
+      <HeroPositionHalf/>
+      <HeroPosition/>
     </>
   );
 };
