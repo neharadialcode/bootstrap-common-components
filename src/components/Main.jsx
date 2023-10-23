eef0bdd775687393ad2b4d0a83af27eff51237bb
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-10-23 12:16:43 +0530

    chng in main.jsx file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,14 +4,14 @@ const Main = () => {
   return (
     <>
       <div className="ps-5 pt-5">
-        <h2 className='pb-3'>HEADER</h2>
+        {/* <h2 className='pb-3'>HEADER</h2> */}
         <a href="/header"className="py-2 px-4 bg-info border-0 text-white">Header</a>
-        <h2 className='pb-3 mt-5'>HERO</h2>
-        <a href="/hero"className="py-2 px-4 bg-info border-0 text-white">Hero</a>
-        <h2 className='pb-3 mt-5'>ABOUT US</h2>
-        <a href="/about"className="py-2 px-4 bg-info border-0 text-white">About Us</a>
-        <h2 className='pb-3 mt-5'>Form</h2>
-        <a href="/form"className="py-2 px-4 bg-info border-0 text-white">Form</a>
+        {/* <h2 className='pb-3 mt-5'>HERO</h2> */}
+        <a href="/hero"className="py-2 px-4 bg-info border-0 text-white ms-3">Hero</a>
+        {/* <h2 className='pb-3 mt-5'>ABOUT US</h2> */}
+        <a href="/about"className="py-2 px-4 bg-info border-0 text-white ms-3">About Us</a>
+        {/* <h2 className='pb-3 mt-5'>Form</h2> */}
+        <a href="/form"className="py-2 px-4 bg-info border-0 text-white ms-3">Form</a>
       </div>
     </>
   );
