420c9017721949f3e656388f3cc39b8c3de3d54c
Sumit chouhan <sumitkhan.dhana@gmail.com>
2023-10-23 09:52:46 +0530

    about progress

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutCenter from "../components/aboutus/AboutCenter";
+import AboutRowCol from "../components/aboutus/AboutRowCol";
 
 const AboutUs = () => {
   return (
     <>
       <AboutCenter />
+      <AboutRowCol/>
     </>
   );
 };
