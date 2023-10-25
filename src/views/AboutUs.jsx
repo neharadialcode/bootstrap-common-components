7655344c6252d2875303f71ba04bc4b0d57988c2
sukhvinder-jaat <monudhaka829@gmail.com>
2023-10-25 10:14:48 +0530

    slider rout

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import AboutCenter from "../components/aboutus/AboutCenter";
 import AboutRowCol from "../components/aboutus/AboutRowCol";
-import AboutSlider from "../components/aboutus/AboutSlider";
 
 const AboutUs = () => {
   return (
     <>
       <AboutCenter />
-      <AboutRowCol/>
-      <AboutSlider/>
+      <AboutRowCol />
     </>
   );
 };
