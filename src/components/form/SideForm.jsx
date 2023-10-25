075fcb0c2e972d6bc083101a7611c8f5b5d46b19
sukhvinder-jaat <monudhaka829@gmail.com>
2023-10-25 09:13:18 +0530

    code merge

--- src/components/form/SideForm.jsx
+++ src/components/form/SideForm.jsx
@@ -1,13 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SideForm = () => {
+  const [touchForm, setTouchForm] = useState({
+    name: "",
+    email: "",
+    contact: "",
+    message: "",
+  });
+  const [error, setError] = useState(false);
+  const regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const OnsubmitHandler = (e) => {
+    e.preventDefault();
+    setError(true);
+    if (
+      touchForm.name !== "" &&
+      touchForm.email !== "" &&
+      touchForm.aboutus !== "" &&
+      touchForm.workup !== "" &&
+      regex.test(touchForm.email)
+    ) {
+      setError(false);
+      setTouchForm({
+        name: "",
+        email: "",
+        aboutus: "",
+        workup: "",
+      });
+        setTimeout(() => {
+          setSubmitVal("Submit Now");
+        }, 3000);
+    }
+  };
   return (
     <>
       <div className="container">
-        <div className="row">
-          <div className="col-6">
-            <div className="SideForm rounded-2 flex-column d-flex justify-content-center">
-              <input type="text" />
+        <div className="row align-items-center flex-lg-row flex-column-reverse">
+          <div className="col-lg-6 col-12">
+            <form
+              onSubmit={(e) => OnsubmitHandler(e)}
+              className="SideForm flex-column d-flex justify-content-center"
+            >
+              <div className="w-100">
+                <input
+                  type="text"
+                  className={`sideFormInput overflow-hidden fs_sm w-100 ${
+                    error && touchForm.name === ""
+                      ? "border-danger"
+                      : touchForm !== ""
+                  }`}
+                  placeholder="Name"
+                  onChange={(e) =>
+                    setTouchForm({ ...touchForm, name: e.target.value })
+                  }
+                  value={touchForm.name}
+                />
+              </div>
+              <input
+                type="text"
+                className="sideFormInput overflow-hidden fs_sm"
+                placeholder="Email address"
+              />
+              <input
+                type="text"
+                className="sideFormInput overflow-hidden fs_sm"
+                placeholder="Contact"
+              />
+              <textarea
+                type="text"
+                className="sideFormInput overflow-hidden fs_sm TextAreaSide overflow-hidden"
+                placeholder="Message"
+              />
+              <button className="text-white d-flex justify-content-center align-items-center submitBtn bg_purple border-0 mt-3">
+                Submit
+              </button>
+            </form>
+          </div>
+          <div className="col-lg-6 col-12 d-lg-flex flex-column align-items-center mb-lg-0 mb-5">
+            <div>
+              <p className=" fs_2xl text-black fw-bold mb-lg-3 mb-1">
+                Get In Touch
+              </p>
+              <p className="text-black opacity-75 paraMaxWidth">
+                Lorem ipsum dolor sit amet consectetur. Velit pulvinar libero
+                porttitor quam sed aliquet. Sodales vulputate elementum arcu
+                donec. Feugiat nibh ullamcorper egestas lectus risus ornare
+                ullamcorper fames. Cursus libero nec facilisis risus malesuada
+                arcu cum.
+              </p>
+              <button className="text-white d-flex justify-content-center align-items-center TouchBtn bg_purple border-0">
+                Get in Touch
+              </button>
             </div>
           </div>
         </div>
--- src/views/Form.jsx
+++ src/views/Form.jsx
@@ -8,10 +8,10 @@ const Form = () => {
       <div className="mb-5 text-center pb-5 pt-5 vh-100 d-flex flex-column justify-content-center">
         <h1 className="mb-5">Full form</h1>
         <FullForm />
-        <h2 className="text-center">Side form⬇⬇⬇</h2>
+        <h2 className="text-center mt-5">Side form⬇⬇⬇</h2>
       </div>
       <div className="vh-100 d-flex flex-column justify-content-center">
-        <h2 className="text-center">Side Form</h2>
+        <h2 className="text-center mb-5">Side Form</h2>
         <SideForm />
       </div>
     </>
