977ca445d5b0c5ea81a7704cbabcf534b8cfab71
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-10-30 17:04:46 +0530

    testing error solving

--- src/components/herosection/HeroCenter.jsx
+++ src/components/herosection/HeroCenter.jsx
@@ -8,15 +8,15 @@ const HeroCenter = () => {
         <div className=" hero_center d-flex ali min-vh100 hero_custom_height align-items-center justify-content-center">
           <div className=" container ">
             <div className=" text-center">
-              <h1 className=" text-center fw-semibold fs_2xl ff_poppins  mx-auto">
+              <h1 className=" text-center fw-bold fs_3xl ff_poppins lh_mainHeading mx-auto">
                 Lorem ipsum Lorem ipsum
               </h1>
-              <p className=" ff_poppins fw-normal fs_sm  mx-auto pt-2 max_w_hero_par mx-auto">
+              <p className=" ff_poppins fw-normal fs_sm  mx-auto pt-2 max_w_hero_par mx-auto lh_All_par">
                 Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget
                 consectetur mi. Vulputate sapien a a bibendum suspendisse quam.
                 Hac posuere eget nam diam velit urna adipiscing diam.
               </p>
-              <button className="ff_poppins fw-normal fs_sm  mx-auto  button_btn">
+              <button className="ff_poppins fw-normal fs_sm  mx-auto  button_btn mt-3 button_lh">
                 Button
               </button>
             </div>
