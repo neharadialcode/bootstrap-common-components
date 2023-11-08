f03d0b5b3b75a4ff4035395e1cb71e3f5f5bfe40
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-11-08 17:56:36 +0530

    minor chng in form and merge code

--- src/views/Form.jsx
+++ src/views/Form.jsx
@@ -7,18 +7,12 @@ const Form = () => {
   return (
     <>
       <div className="mb-5 text-center pb-5 pt-5 min-vh-100 d-flex flex-column justify-content-center">
-        <h1 className="mb-5 pt-5">Full form</h1>
         <FullForm />
-        <h2 className="text-center mt-5 pt-5 d-md-block d-none">
-          Side form⬇⬇⬇
-        </h2>
-      </div>
+             </div>
       <div className="min-vh-100 d-flex flex-column justify-content-center">
-        <h2 className="text-center mb-5 pb-5 pt-5">Side Form</h2>
         <SideForm />
       </div>
       <div className="min-vh-100 d-flex flex-column justify-content-center">
-        <h2 className="text-center pt-5">Center Form</h2>
         <CenterForm />
       </div>
     </>
