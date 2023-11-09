0470516eba39d58cae826945ef0a6a7d80201c89
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-11-09 15:41:05 +0530

    error solving

--- src/components/footer/FooterOne.jsx
+++ src/components/footer/FooterOne.jsx
@@ -70,7 +70,7 @@ const FooterOne = () => {
                 <p className="font-sm ff_poppins text-black">Get in Touch</p>
                 <form>
                   <div className="mt-0">
-                    <div className="footer_input_clr  ps-4 pe-1 py-1 d-flex align-items-center">
+                    <div className="footer_input_clr  ps-2 pe-1 py-1 d-flex align-items-center">
                       <input
                         type="text"
                         placeholder="Type Your Mail"
