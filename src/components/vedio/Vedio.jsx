051e555333f1bc73e5bd9036ffe8e8cd6ffc6027
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-11-09 15:59:34 +0530

    code error solving

--- src/components/vedio/Vedio.jsx
+++ src/components/vedio/Vedio.jsx
@@ -1,12 +1,14 @@
   import React from 'react'
-  import vedioRandom from "../../assetsFolder/images/video/crescent_moon_-_27186 (1080p).mp4";
+  import crescentMoon from "../../assetsFolder/images/video/crescent_moon.mp4";
   const Vedio = () => {
     return (
       <>
         <div className="container mt-5">
           <div className=" position-relative">
-            <p className=' fs_xl fw-bold position-absolute text-white ms-4 mt-2'>Lorem: ipsum dolor sit</p>
-            <iframe src={vedioRandom} className="w-100 min_h_vedio m-0" />
+            <p className=" fs_xl fw-bold position-absolute text-white ms-4 mt-2">
+              Lorem: ipsum dolor sit
+            </p>
+            <iframe src={crescentMoon} className="w-100 min_h_vedio m-0" />
           </div>
         </div>
       </>
