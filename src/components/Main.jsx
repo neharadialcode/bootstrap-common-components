6040cf2537363438ee769e05cfa01f4c91b35bb0
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-11-10 14:23:55 +0530

    remove extra code and helper spell mistake correct

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,19 +4,15 @@ const Main = () => {
   return (
     <>
       <div className="pt-5 d-flex align-items-center flex-wrap gap-4 justify-content-center">
-        {/* <h2 className='pb-3'>HEADER</h2> */}
         <a href="/header" className="py-2 px-4 bg-info border-0 text-white">
           Header
         </a>
-        {/* <h2 className='pb-3 mt-5'>HERO</h2> */}
         <a href="/hero" className="py-2 px-4 bg-info border-0 text-white">
           Hero
         </a>
-        {/* <h2 className='pb-3 mt-5'>ABOUT US</h2> */}
         <a href="/about" className="py-2 px-4 bg-info border-0 text-white">
           About Us
         </a>
-        {/* <h2 className='pb-3 mt-5'>Form</h2> */}
         <a href="/form" className="py-2 px-4 bg-info border-0 text-white">
           Form
         </a>
